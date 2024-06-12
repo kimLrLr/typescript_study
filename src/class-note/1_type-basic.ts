da884fdeec4134bf2ace148aaea3dd83e22ff19f
Harim Kim <rimiha9708@gmail.com>
2024-06-12 23:32:03 +0900

    인터페이스

--- src/class-note/1_type-basic.ts
+++ src/class-note/1_type-basic.ts
@@ -1,32 +1,32 @@
-// JS 문자열 선언
-// var str = "hello";
+// // JS 문자열 선언
+// // var str = "hello";
 
-// TS 문자열 선언
-// var str: string = "hello";
-let str: string = "hello";
+// // TS 문자열 선언
+// // var str: string = "hello";
+// let str: string = "hello";
 
-//TS 숫자
-let num: number = 10;
-// es6 이상을 사용하는 때에는 변수 var을 대신해서 let이나 const를 사용
+// //TS 숫자
+// let num: number = 10;
+// // es6 이상을 사용하는 때에는 변수 var을 대신해서 let이나 const를 사용
 
-//TS 배열
-let arr: Array<number> = [1, 2, 3];
-let heroes: Array<string> = ["kimLrLr", "choiLrLr", "BaeLrLr"];
-let items: number[] = [1, 2, 3];
+// //TS 배열
+// let arr: Array<number> = [1, 2, 3];
+// let heroes: Array<string> = ["kimLrLr", "choiLrLr", "BaeLrLr"];
+// let items: number[] = [1, 2, 3];
 
-// TS 튜플: 배열의 각각 index에 타입이 정의되어 있다.
-let address: [string, number] = ["gangnam", 100];
+// // TS 튜플: 배열의 각각 index에 타입이 정의되어 있다.
+// let address: [string, number] = ["gangnam", 100];
 
-// TS 객체
-let obj: object = {};
-// let person: object = {
-//     name: 'LrLr',
-//     age: 17
+// // TS 객체
+// let obj: object = {};
+// // let person: object = {
+// //     name: 'LrLr',
+// //     age: 17
+// // };
+// let person: { name: string; age: number } = {
+//   name: "LrLr",
+//   age: 17,
 // };
-let person: { name: string; age: number } = {
-  name: "LrLr",
-  age: 17,
-};
 
-// TS 진위값
-let show: boolean = true;
+// // TS 진위값
+// let show: boolean = true;
--- src/class-note/2_functions.ts
+++ src/class-note/2_functions.ts
@@ -1,22 +1,22 @@
-// 함수의 파라미터에 타입을 정의하는 방식
-function sum(a: number, b: number) {
-  return a + b;
-}
-sum(10, 20);
+// // 함수의 파라미터에 타입을 정의하는 방식
+// // function sum(a: number, b: number) {
+// //   return a + b;
+// // }
+// // sum(10, 20);
 
-// 함수의 반환 값에 타입을 정의하는 방식
-function add(): number {
-  return 10;
-}
+// // 함수의 반환 값에 타입을 정의하는 방식
+// function add(): number {
+//   return 10;
+// }
 
-// 함수에 타입을 정의하는 방식 종합
-function result(a: number, b: number): number {
-  return a + b;
-}
-result(10, 20);
+// // 함수에 타입을 정의하는 방식 종합
+// function result(a: number, b: number): number {
+//   return a + b;
+// }
+// result(10, 20);
 
-// 함수의 옵셔널 파라미터
-function log(a: string, b: string, c?: string) {}
-// log('hello world');
-log("hello ts", "banana");
-log("hello ts", "banana", "kimLrLr");
+// // 함수의 옵셔널 파라미터
+// function log(a: string, b: string, c?: string) {}
+// // log('hello world');
+// log("hello ts", "banana");
+// log("hello ts", "banana", "kimLrLr");
