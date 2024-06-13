78aa41442fc42f9b41020fb154ed5504d2cb1eb3
Harim Kim <rimiha9708@gmail.com>
2024-06-13 17:01:26 +0900

    연산자를 이용한 타입(유니온, 인터섹션 타입)

--- src/class-note/4_type-aliases.ts
+++ src/class-note/4_type-aliases.ts
@@ -1,22 +1,22 @@
-// interface Person {
+// // interface Person {
+// //     name: string;
+// //     age: number;
+// // }
+
+// type Person = {
 //     name: string;
 //     age: number;
 // }
 
-type Person = {
-    name: string;
-    age: number;
-}
+// var lrlr: Person ={
+//     name: '나나',
+//     age: 100
+// }
 
-var lrlr: Person ={
-    name: '나나',
-    age: 100
-}
+// type MyString = string;
+// var str: MyString = 'hello';
 
-type MyString = string;
-var str: MyString = 'hello';
+// type Todo = {id: string; title: string; done: boolean};
+// function getTodo(todo: Todo){
 
-type Todo = {id: string; title: string; done: boolean};
-function getTodo(todo: Todo){
-
-}
+// }
