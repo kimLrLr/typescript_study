8376e2cd940719c068330eaa4de15bb88f74c4ee
Harim Kim <rimiha9708@gmail.com>
2024-06-13 23:50:56 +0900

    제네릭 타입 제한

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,31 @@
-const emails = [
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem<T extends string | number>(item: DropdownItem<T>,): HTMLOptionElement {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -20,7 +35,12 @@ function createDropdownItem(item) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
-  selectTag.appendChild(item);
+  // selectTag.appendChild(item);
 });
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem<number>(product);
+});
+
--- src/class-note/8_generics.ts
+++ src/class-note/8_generics.ts
@@ -1,48 +1,94 @@
-// // // JS 코드
-// // function logText(text) {
+// // // // // JS 코드
+// // // // function logText(text) {
+// // // //     console.log(text);
+// // // //     return text;
+// // // // }
+// // // // logText(10);    // 숫자 10
+// // // // logText('하이'); // 문자열 하이
+// // // // logText(true);  // 진위값 true
+
+// // // function logText<T>(text: T): T {
+// // //     console.log(text);
+// // //     return text;
+// // // }
+// // // // 함수를 호출할때 파라미터의 타입을 지정하며 넘길 수 있다.
+// // // logText<string>('하이');
+// // // logText<number>(100);
+
+// // // function logText(text: string) {
+// // //     console.log(text);
+// // //     text.split('').reverse().join('');
+// // //     return text;
+// // // }
+
+// // // function logNumber(num: number) {
+// // //     console.log(num);
+// // //     return num;
+// // // }
+
+// // // function logText(text: string | number) {
+// // //     console.log(text);
+// // //     return text;
+// // // }
+
+// // // const a = logText('a');
+// // // logText(10);
+// // // const num = logNumber(10);
+
+// // function logText<T>(text: T): T {
 // //     console.log(text);
 // //     return text;
 // // }
-// // logText(10);    // 숫자 10
-// // logText('하이'); // 문자열 하이
-// // logText(true);  // 진위값 true
 
-// function logText<T>(text: T): T {
-//     console.log(text);
-//     return text;
+// // const str = logText<string>('abc');
+// // str.split('');
+// // const login = logText<boolean>(true);
+
+// // // logText('a');
+// // // logText(10);
+
+// // 인터페이스에 제네릭을 선언하는 방법
+// // interface Dropdown {
+// //     value: string;
+// //     selected: boolean;
+// // }
+
+// // interface Dropdown<T> {
+// //     value: T;
+// //     selected: boolean;
+// //     }
+
+// // const obj: Dropdown<string> = { value: 'abc', selected: false };
+
+// // 제네릭의 타입 제한
+// // function logTextLength<T>(text: T[]): T[] {
+// //     console.log(text.length);
+// //     text.forEach(function (text) {
+// //         console.log(text);
+// //     })
+// //     return text;
+// // }
+// // logTextLength(['hi', 'abc']);
+
+// // 제네릭 타입 제한 2 - 정의된 타입 이용하기
+// // interface LengthType {
+// //     length: number;
+// // }
+// // function logTextLength<T extends LengthType>(text: T): T {
+// //     text.length;
+// //     return text;
+// // }
+// // logTextLength(10);
+// // logTextLength({length: 10});
+
+// // 제네릭 타입 제한 3 - keyof
+// interface ShoppingItem {
+//     name: string;
+//     price: number;
+//     stock: number;
 // }
-// // 함수를 호출할때 파라미터의 타입을 지정하며 넘길 수 있다.
-// logText<string>('하이');
-// logText<number>(100);
 
-// function logText(text: string) {
-//     console.log(text);
-//     text.split('').reverse().join('');
-//     return text;
+// function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T{
+//     return itemOption;
 // }
-
-// function logNumber(num: number) {
-//     console.log(num);
-//     return num;
-// }
-
-// function logText(text: string | number) {
-//     console.log(text);
-//     return text;
-// }
-
-// const a = logText('a');
-// logText(10);
-// const num = logNumber(10);
-
-function logText<T>(text: T): T {
-    console.log(text);
-    return text;
-}
-
-const str = logText<string>('abc');
-str.split('');
-const login = logText<boolean>(true);
-
-// logText('a');
-// logText(10);
+// getShoppingItemOption("name");
