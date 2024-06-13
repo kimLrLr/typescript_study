9a6c3d4c3c8c3c9fa62f601ea8ebf440883dff76
Harim Kim <rimiha9708@gmail.com>
2024-06-13 21:06:19 +0900

    프로토타입과 클래스

---
+++ src/class-note/class.js
@@ -0,0 +1,19 @@
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// var lrlr2 = new Person ('LrLr', 100);
+
+// // ES2015 (ES6)
+// class Person{
+//     // 클래스 로직
+//     constructor(name, age) { //  초기화 메서드
+//         console.log('생성 되었습니다');
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// var lrlr = new Person('나나', 30);   // 생성 되었습니다.
+// console.log(lrlr);
