3af0841968836c15d82cd6267d321347594c9cfd
Harim Kim <rimiha9708@gmail.com>
2024-06-13 17:25:42 +0900

    Enums

--- src/class-note/5_operator.ts
+++ src/class-note/5_operator.ts
@@ -1,47 +1,47 @@
-// function logMessage(value: any) {
-//     console.log(value);
+// // function logMessage(value: any) {
+// //     console.log(value);
+// // }
+// // logMessage('hello');
+// // logMessage(100);
+
+// var lrlr: string | number | boolean;
+// function logMessage(value: string | number){
+//    if (typeof value === 'number') {
+//     value.toLocaleString();
+//    }
+//    if (typeof value === 'string') {
+//     value.toString();
+//    }
+//    throw new TypeError('value must be string or number');
 // }
 // logMessage('hello');
 // logMessage(100);
 
-var lrlr: string | number | boolean;
-function logMessage(value: string | number){
-   if (typeof value === 'number') {
-    value.toLocaleString();
-   }
-   if (typeof value === 'string') {
-    value.toString();
-   }
-   throw new TypeError('value must be string or number');
-}
-logMessage('hello');
-logMessage(100);
-
-interface Developer {
-    name: string;
-    skill: string;
-}
-
-interface Person {
-    name: string;
-    age: number;
-}
-
-// function askSomeone(someone: Developer | Person) {
-//     // someone.name;
-//     // someone.skill;
-//     // someone.age;
+// interface Developer {
+//     name: string;
+//     skill: string;
 // }
 
-function askSomeone(someone: Developer & Person) {
-    someone.name;
-    someone.age;
-    someone.skill;
-    }
+// interface Person {
+//     name: string;
+//     age: number;
+// }
 
-askSomeone({ name: '개발자', skill: '웹 개발', age: 100 });
-// askSomeone({ name: '나나', age: 100 });
+// // function askSomeone(someone: Developer | Person) {
+// //     // someone.name;
+// //     // someone.skill;
+// //     // someone.age;
+// // }
 
-// 인터섹션 타입 &
-var lrlr: string | number | boolean;
-var kim: string & number & boolean;
+// function askSomeone(someone: Developer & Person) {
+//     someone.name;
+//     someone.age;
+//     someone.skill;
+//     }
+
+// askSomeone({ name: '개발자', skill: '웹 개발', age: 100 });
+// // askSomeone({ name: '나나', age: 100 });
+
+// // 인터섹션 타입 &
+// var lrlr: string | number | boolean;
+// var kim: string & number & boolean;
