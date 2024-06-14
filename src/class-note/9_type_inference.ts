d9ba7ed9a1072e1892a3b39dc623a8958249575b
Harim Kim <rimiha9708@gmail.com>
2024-06-14 17:23:58 +0900

    맵드 타입

--- src/class-note/9_type_inference.ts
+++ src/class-note/9_type_inference.ts
@@ -1,37 +1,37 @@
-// 타입 추론 기본 1
-// var a = 'abc';
+// // 타입 추론 기본 1
+// // var a = 'abc';
 
-// function getB(b = 10) {
-//     var c = 'hi';
-//     return b + c;   // 10 + '10' 이니 문자열 1010 출력
-// }
+// // function getB(b = 10) {
+// //     var c = 'hi';
+// //     return b + c;   // 10 + '10' 이니 문자열 1010 출력
+// // }
 
-// 타입 추론 기본 2
+// // 타입 추론 기본 2
+// // interface Dropdown<T> {
+// //     value: T;
+// //     title: string;
+// // }
+// // var shoppingItem: Dropdown<string> = {
+// //     value: 'abc',
+// //     title: 'hello'
+// // }
+
+// // 타입 추론 기본 3
 // interface Dropdown<T> {
 //     value: T;
 //     title: string;
 // }
-// var shoppingItem: Dropdown<string> = {
-//     value: 'abc',
-//     title: 'hello'
+// interface DetailedDropdown<K> extends Dropdown<K> {
+//     description: string;
+//     tag: K;
 // }
 
-// 타입 추론 기본 3
-interface Dropdown<T> {
-    value: T;
-    title: string;
-}
-interface DetailedDropdown<K> extends Dropdown<K> {
-    description: string;
-    tag: K;
-}
+// var detailedItem : DetailedDropdown<number> = {
+//     title: 'abc',
+//     description: 'ab',
+//     value: 123,
+//     tag: 456
+// };
 
-var detailedItem : DetailedDropdown<number> = {
-    title: 'abc',
-    description: 'ab',
-    value: 123,
-    tag: 456
-};
-
-// Best Common Type
-var arr = [1, 2, true, true, 'a'];
+// // Best Common Type
+// var arr = [1, 2, true, true, 'a'];
