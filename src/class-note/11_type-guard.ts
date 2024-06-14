9374fc26505a268d394351952f3c4fd201c203de
Harim Kim <rimiha9708@gmail.com>
2024-06-14 14:17:32 +0900

    타입 호환

--- src/class-note/11_type-guard.ts
+++ src/class-note/11_type-guard.ts
@@ -1,34 +1,34 @@
-interface Developer {
-    name: string;
-    skill: string;
-}
+// interface Developer {
+//     name: string;
+//     skill: string;
+// }
     
-interface Person {
-    name: string;
-    age: number;
-}
+// interface Person {
+//     name: string;
+//     age: number;
+// }
 
-function introduce(): Developer | Person {
-    return { name: 'LrLr', age: 100, skill: 'programing'}
-}
-var kim = introduce();
-// console.log(kim.skill);
+// function introduce(): Developer | Person {
+//     return { name: 'LrLr', age: 100, skill: 'programing'}
+// }
+// var kim = introduce();
+// // console.log(kim.skill);
 
-if ((kim as Developer).skill) {
-    var skill = (kim as Developer).skill;
-    console.log(skill);
-} else if ((kim as Person).age) {
-    var age = (kim as Person).age;
-    console.log(age);
-}
+// if ((kim as Developer).skill) {
+//     var skill = (kim as Developer).skill;
+//     console.log(skill);
+// } else if ((kim as Person).age) {
+//     var age = (kim as Person).age;
+//     console.log(age);
+// }
 
-// 타입 가드 정의
-function isDeveloper(target: Developer | Person): target is Developer {
-    return (target as Developer).skill !== undefined
-}
+// // 타입 가드 정의
+// function isDeveloper(target: Developer | Person): target is Developer {
+//     return (target as Developer).skill !== undefined
+// }
 
-if (isDeveloper(kim)) {
-    console.log(kim.skill);
-} else{
-    console.log(kim.age);
-}
+// if (isDeveloper(kim)) {
+//     console.log(kim.skill);
+// } else{
+//     console.log(kim.age);
+// }
